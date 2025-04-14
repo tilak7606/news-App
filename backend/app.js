8cc02f956ba47dead063fc7a757b6d88f9696c79
tilak7606 <tilak113saini@gmail.com>
2025-04-14 22:15:19 +0530

    backend added successfully

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+// console.log("hey from backend")
+const cors = require('cors');
+app.use(cors());
+
+console.log("hey ")
+
+app.get('/api/news',async function(req,res){
+
+    
+    const {q} = req.query;
+    const response = await fetch(`https://newsapi.org/v2/everything?q=${q}&apiKey=${process.env.API_KEY}`);
+    const jsonData = await response.json();
+    // console.log(jsonData.articles,"from app.js")
+    res.send(jsonData.articles);
+})
+
+
+app.listen(3000);
